7960b23a4cca9a052b925568c76e419962778255
Emmil Ayton <ej.ayitey@gmail.com>
2023-10-17 22:26:54 +0000

    Added 'starting next' to second text read file process & changed 'starting next' on first text read file process to 'starting new'

--- 01-node-tutorial/11-fs-async.js
+++ 01-node-tutorial/11-fs-async.js
@@ -24,6 +24,7 @@ readFile('./content/first.txt', 'utf8', (err, result) => {
         console.log('done with this task')
       }
     )
-  })
+  }) 
+  console.log('starting next task')
 })
-console.log('starting next task')
+console.log('starting new task')
